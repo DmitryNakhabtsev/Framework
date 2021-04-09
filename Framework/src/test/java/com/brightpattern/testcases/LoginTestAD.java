3cad144460356b0e838d3910b85d5aa2ae175f91
DmitryNakhabtsev <dmitry.nakhabtsev@brightpattern.com>
2021-04-09 16:18:40 -0700

    final

--- Framework/src/test/java/com/brightpattern/testcases/LoginTestAD.java
+++ Framework/src/test/java/com/brightpattern/testcases/LoginTestAD.java
@@ -1,6 +1,11 @@
 package com.brightpattern.testcases;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -12,61 +17,43 @@ import com.brightpattern.utility.BrowserFactory;
 import com.brightpattern.utility.ExcelDataProvider;
 import com.brightpattern.utility.Helper;
 
-public class LoginTestAD extends BaseClass {
+public class LoginTestAD {
 	
 	@Test(priority = 1)
 	public void loginApp() {
 		
-		logger = report.createTest("Login to Brightpattern AD");
-		
-		LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
-		
-		logger.info("Starting Application");
-		
-		loginPage.loginToAD(excel.getStringData("Login",0,0), excel.getStringData("Login",0,1));
-		
-		logger.pass("Login Success");
-	
-	}
-	
-	@Test(priority = 2)
-	public void loginApp_aj() {
-		
-		logger = report.createTest("Login to Brightpattern alan.jenks AD");
-		
-		LoginPage loginPage = PageFactory.initElements(driver_aj, LoginPage.class);
-		
-		logger.info("Starting Application alan.jenks");
-		
-		loginPage.loginToAD(excel.getStringData("Login",1,0), excel.getStringData("Login",1,1));
-		
-		logger.pass("Login Success");
-	
-	}	
-	
-	@Test(priority = 3)
-	public void loginApp_tb() {
-		
-		logger = report.createTest("Login to Brightpattern tony.cobb AD");
-		
-		LoginPage loginPage = PageFactory.initElements(driver_tb, LoginPage.class);
-		
-		logger.info("Starting Application tony.cobb");
-		
-		loginPage.loginToAD(excel.getStringData("Login",2,0), excel.getStringData("Login",2,1));
-		
-		logger.pass("Login Success");
-	
-	}
 
-	@Test(priority = 4)
-	public void loginApp1() {
-		
-		logger = report.createTest("Logout");
-		
-		logger.fail("Logout failed");
+		String agentName = "admin";
+		String agentPassword = "password";
+		System.setProperty("webdriver.chrome.driver", "C:\\tools\\chrome\\chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();
+		WebDriver driver;
+
+		options.addArguments("headless");
+
+//		  options.addExtensions(new File("C:\\Users\\admin\\Documents\\AUTOMATION\\Agent Desktop Chrome Extension 1.16.0.0.crx")); // BPClient chrome extension
+		  options.addArguments("--disable-notifications");
+		  options.addArguments("use-fake-ui-for-media-stream"); // microphone access in the agent desktop
+		  
+		driver = new ChromeDriver(options);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+
+		driver.get("https://dima1.ssf.bugfocus.com/agentdesktop/");
+		System.out.println("Browser started");
+		driver.get("https://autotests.ssf.bugfocus.com/agentdesktop/");
+		driver.findElement(By.xpath("//*[@id=\"auth-login\"]")).sendKeys(agentName); // AGENT DESKTOP LOGIN: user name
+		driver.findElement(By.xpath("//*[@id=\"auth-password\"]")).sendKeys(agentPassword); // AGENT DESKTOP LOGIN: password
+		driver.findElement(By.xpath("//*[@id=\"auth-submit\"]")).click();
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		driver.quit();
+		System.out.println("Browser closed");
 	
 	}
-
 	
+
 }
---
+++ Framework/src/test/java/com/brightpattern/testcases/LoginTestADOriginal.java
@@ -0,0 +1,72 @@
+package com.brightpattern.testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.brightpattern.pages.BaseClass;
+import com.brightpattern.pages.LoginPage;
+import com.brightpattern.utility.BrowserFactory;
+import com.brightpattern.utility.ExcelDataProvider;
+import com.brightpattern.utility.Helper;
+
+public class LoginTestADOriginal extends BaseClass {
+	
+	@Test(priority = 1)
+	public void loginApp() {
+		
+		logger = report.createTest("Login to Brightpattern AD");
+		
+		LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+		
+		logger.info("Starting Application");
+		
+		loginPage.loginToAD(excel.getStringData("Login",0,0), excel.getStringData("Login",0,1));
+		
+		logger.pass("Login Success");
+	
+	}
+	
+	@Test(priority = 2, enabled = true)
+	public void loginApp_aj() {
+		
+		logger = report.createTest("Login to Brightpattern alan.jenks AD");
+		
+		LoginPage loginPage = PageFactory.initElements(driver_aj, LoginPage.class);
+		
+		logger.info("Starting Application alan.jenks");
+		
+		loginPage.loginToAD(excel.getStringData("Login",1,0), excel.getStringData("Login",1,1));
+		
+		logger.pass("Login Success");
+	
+	}	
+	
+	@Test(priority = 3, enabled = true)
+	public void loginApp_tb() {
+		
+		logger = report.createTest("Login to Brightpattern tony.cobb AD");
+		
+		LoginPage loginPage = PageFactory.initElements(driver_tb, LoginPage.class);
+		
+		logger.info("Starting Application tony.cobb");
+		
+		loginPage.loginToAD(excel.getStringData("Login",2,0), excel.getStringData("Login",2,1));
+		
+		logger.pass("Login Success");
+	
+	}
+
+	@Test(priority = 4, enabled = true)
+	public void loginApp1() {
+		
+		logger = report.createTest("Logout");
+		
+		logger.fail("Logout failed");
+	
+	}
+
+	
+}
