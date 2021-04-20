1e16d37bc3b4f5f049490b5f1368029d87862c83
DmitryNakhabtsev <dmitry.nakhabtsev@brightpattern.com>
2021-04-20 10:35:00 -0700

    switch to autotests host

--- Framework/src/test/java/com/brightpattern/testcases/API.java
+++ Framework/src/test/java/com/brightpattern/testcases/API.java
@@ -109,15 +109,15 @@ public class API {
 		options.addArguments("--headless");		
 //		options.addExtensions(new File("C:\\Users\\admin\\Documents\\AUTOMATION\\Agent Desktop Chrome Extension 1.16.0.0.crx")); // BPClient chrome extension
 		options.addArguments("--disable-notifications");
-//		options.addArguments("use-fake-ui-for-media-stream"); // microphone access in the agent desktop
+		options.addArguments("use-fake-ui-for-media-stream"); // microphone access in the agent desktop
 		
-		options.addArguments("--use-fake-device-for-media-stream"); //Bypass the media stream infobar by selecting the default device for media streams (e.g. WebRTC)
+//		options.addArguments("--use-fake-device-for-media-stream"); //Bypass the media stream infobar by selecting the default device for media streams (e.g. WebRTC)
 		
 	    options.addArguments("--verbose");
 	    options.addArguments("--whitelisted-ips=''");
 	    
-	    String hostURL = "https://dima1.ssf.bugfocus.com/agentdesktop/";
-//	    String hostURL = "https://autotests.ssf.bugfocus.com/agentdesktop/";
+//	    String hostURL = "https://dima1.ssf.bugfocus.com/agentdesktop/";
+	    String hostURL = "https://autotests.ssf.bugfocus.com/agentdesktop/";
 		
 		switch(agentName) {
 		  case "alan.jenks":
