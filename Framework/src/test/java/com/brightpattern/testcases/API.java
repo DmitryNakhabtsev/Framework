f65db3ced457db45a62d69a8f44421c89c466715
DmitryNakhabtsev <dmitry.nakhabtsev@brightpattern.com>
2021-04-20 17:45:35 -0700

    no-sandbox

--- Framework/src/test/java/com/brightpattern/testcases/API.java
+++ Framework/src/test/java/com/brightpattern/testcases/API.java
@@ -110,6 +110,8 @@ public class API {
 //		options.addExtensions(new File("C:\\Users\\admin\\Documents\\AUTOMATION\\Agent Desktop Chrome Extension 1.16.0.0.crx")); // BPClient chrome extension
 		options.addArguments("--disable-notifications");
 //		options.addArguments("use-fake-ui-for-media-stream"); // microphone access in the agent desktop
+
+		options.addArguments("--no-sandbox");	
 		
 		options.addArguments("--use-fake-device-for-media-stream"); //Bypass the media stream infobar by selecting the default device for media streams (e.g. WebRTC)
 		
