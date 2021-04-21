9181cfd818fcfd301f4ffcd5e645c10558006333
DmitryNakhabtsev <dmitry.nakhabtsev@brightpattern.com>
2021-04-21 11:27:23 -0700

    simple inbound call scenario

--- Framework/src/test/java/com/brightpattern/testcases/DimaAfter.java
+++ Framework/src/test/java/com/brightpattern/testcases/DimaAfter.java
@@ -19,7 +19,7 @@ public class DimaAfter extends API{
 		agentInit(agent1, pwd);
 //		setServiceToAgent(agent1);
 		
-		System.out.println("Started initalization for tony.cobb");
+//		System.out.println("Started initalization for tony.cobb");
 		agentInit(agent2, pwd);
 //		setServiceToAgent(agent2);
 
@@ -43,8 +43,8 @@ public class DimaAfter extends API{
 	
 		agentCallAnswer(agent1);                // Agent1 accepts the call 
 		wait(callDuration+callDuration);  		// Agent1 talks to Customer for 2*callDuration seconds
-		checkHoldButton(agent1); 
-		wait(action);  							//Agent1 puts call on Hold for 10 seconds
+//		checkHoldButton(agent1); 
+//		wait(action);  							//Agent1 puts call on Hold for 10 seconds
 
 		agentConsultCall(agent1, "2023");       // Agent1 takes consult call from Hold 		
 		wait(action);  							// In 10 seconds Agent1 places a consult call to Agent2
@@ -52,19 +52,19 @@ public class DimaAfter extends API{
 		wait(action);  							// Agent1 and Agent2 talks for 10 seconds	
 		
 		agentCompleteTransfer(agent1);          // "Complete Transfer" button
-		wait(3*callDuration);  					// Agent1 talks to Customer for 3*callDuration seconds
+//		wait(3*callDuration);  					// Agent1 talks to Customer for 3*callDuration seconds
 		
-		checkHoldButton(agent2); 
-		wait(action);  							// Agent2 puts call on hold for 10 seconds	
-		checkRetrieveButton(agent2); 			// Agent2 takes call from Hold
+//		checkHoldButton(agent2); 
+//		wait(action);  							// Agent2 puts call on hold for 10 seconds	
+//		checkRetrieveButton(agent2); 			// Agent2 takes call from Hold
 		
 		wait(4*callDuration);  					// Agent1 talks to Customer for 4*callDuration seconds 
 		
 		agentEndCall(admin);                    // Customer hangs up the call
 		
 		wait(action);  							// Agent2 sets up disposition and completes interaction in 10 seconds
-		selectDisposition(agent2);
-		selectDisposition(agent1);
+//		selectDisposition(agent2);
+//		selectDisposition(agent1);
 	
 		agentLogOut(agent1);
 		agentLogOut(agent2);
---
+++ Framework/src/test/java/com/brightpattern/testcases/LoginTestADJenkins.java
@@ -0,0 +1,51 @@
+package com.brightpattern.testcases;
+
+import java.util.Arrays;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.brightpattern.utility.Helper;
+public class LoginTestADJenkins extends API {
+	
+
+	
+	@Test(priority = -3)
+	public void ArrayBefore() {
+
+		agentInit("admin","password");
+		agentInit("alan.jenks","password");
+		wait(2000);
+		
+		agentCallTo("admin", "2022");
+		wait(2000);
+		System.out.println(Helper.captureScreenshot(driver));
+		
+//		String str = driver_aj.getPageSource();
+////		System.out.println(str);	
+//		System.out.println("Page Source Length is: " + str.length());
+//
+//		if (str.contains("Bright Pattern")) {
+//			System.out.println("Pass");
+//		} else {
+//			System.out.println("Fail");
+//		}
+		
+//		wait(1000);		
+		
+		agentCallAnswer("alan.jenks");
+		wait(2000);
+		System.out.println(Helper.captureScreenshot(driver_aj));
+		
+		agentEndCall("alan.jenks");
+//		agentEndCall("admin");		
+		wait(2000);
+		System.out.println(Helper.captureScreenshot(driver));
+		agentLogOut("admin");				
+		agentLogOut("alan.jenks");
+
+
+	}
+	
+}
