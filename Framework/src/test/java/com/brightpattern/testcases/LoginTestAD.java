5c109b8f83c4231b9d0c2e3b120a9a4c7e16d25e
DmitryNakhabtsev <dmitry.nakhabtsev@brightpattern.com>
2021-04-21 17:01:57 -0700

    expanded scenario

--- Framework/src/test/java/com/brightpattern/testcases/LoginTestAD.java
+++ Framework/src/test/java/com/brightpattern/testcases/LoginTestAD.java
@@ -9,42 +9,53 @@ import org.testng.annotations.Test;
 import com.brightpattern.utility.Helper;
 public class LoginTestAD extends API {
 	
+	//getServicesMetrics
+	protected int serviceMetricNumber; //Service Metric index in array
+	protected static String[][] serviceMetricsBefore;
+	protected static String[][] serviceMetricsAfter;
+	
+	String agent1 = "alan.jenks";
+	String agent2 = "tony.cobb";
+	String admin = "admin";
+	String pwd = "password";
 
+	int callDuration = 3000; // 10 sec for every conversation
+	int action = 1000; 
+	
 	
 	@Test(priority = -3)
 	public void ArrayBefore() {
 
 		agentInit("admin","password");
-		agentInit("alan.jenks","password");
-		wait(2000);
+		serviceMetricsBefore = getServicesMetrics("Service A", 3);	
+//		agentMetricsBefore = getAgentsMetrics("admin", 1);	
+//		teamMetricsBefore = getTeamsMetrics("TeamDmitry");
+		System.out.println(Helper.captureScreenshot(driver_aj));
+		printOutMetrics(serviceMetricsBefore);
 		
-		agentCallTo("admin", "2022");
-		wait(2000);
-		System.out.println(Helper.captureScreenshot(driver));
-		
-//		String str = driver_aj.getPageSource();
-////		System.out.println(str);	
-//		System.out.println("Page Source Length is: " + str.length());
-//
-//		if (str.contains("Bright Pattern")) {
-//			System.out.println("Pass");
-//		} else {
-//			System.out.println("Fail");
-//		}
-		
-//		wait(1000);		
-		
-		agentCallAnswer("alan.jenks");
-		wait(2000);
+		agentInit(agent1, pwd);
+		setServiceToAgent(agent1);
 		System.out.println(Helper.captureScreenshot(driver_aj));
 		
-		agentEndCall("alan.jenks");
-//		agentEndCall("admin");		
+		agentInit(agent2, pwd);
+		setServiceToAgent(agent2);
+		System.out.println(Helper.captureScreenshot(driver_tb));
+		
+		agentCallTo(admin, "2022");
 		wait(2000);
+		System.out.println(Helper.captureScreenshot(driver));	
+		
+		agentCallAnswer(agent1);
+//		wait(2000);
+		System.out.println(Helper.captureScreenshot(driver_aj));
+		
+		agentEndCall(agent1);
+//		agentEndCall("admin");		
+//		wait(2000);
 		System.out.println(Helper.captureScreenshot(driver));
-		agentLogOut("admin");				
-		agentLogOut("alan.jenks");
-
+		agentLogOut(admin);				
+		agentLogOut(agent1);
+		agentLogOut(agent2);
 
 	}
 	
