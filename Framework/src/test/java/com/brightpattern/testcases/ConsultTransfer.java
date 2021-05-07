1f653f2713653d0ffcce948a853467765ec5f5eb
DmitryNakhabtsev <dmitry.nakhabtsev@brightpattern.com>
2021-05-07 09:47:33 -0700

    actual = null;

--- Framework/src/test/java/com/brightpattern/testcases/ConsultTransfer.java
+++ Framework/src/test/java/com/brightpattern/testcases/ConsultTransfer.java
@@ -126,7 +126,7 @@ test_089_ServiceA_INHandled				IN Handled				Inbound interactions handled by age
 	@Test(priority = -4)
 	public void DimaAssert() {
 		String expected = "dima";
-		String actual = "olga";
+		String actual = null;
 		assertNotNull(expected, actual);
 	}
 	
